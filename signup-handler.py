d3cf8882ca5b128e86cf29f0414370a8fa7438d6
Austin <austin@blockseer.com>
2018-07-31 09:44:14 -0700

    Update

--- signup-handler.py
+++ signup-handler.py
@@ -33,7 +33,7 @@ if not table_exists(conn, "signup"):
 @app.route('/', methods=['POST', 'GET'])
 def foo():
     print("RECIEVED")
-    data = request.get_json(force=True)
+    data = request.values.to_dict()
 
     print(data)
     return "OK"
